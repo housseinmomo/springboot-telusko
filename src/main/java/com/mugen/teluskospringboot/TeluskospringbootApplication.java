ec4591f6cff94692fc8f882b63d79f4f00001ed2
housseinmomo <smrobla34@gmail.com>
2022-09-30 12:35:16 +0200

    initial commit

---
+++ src/main/java/com/mugen/teluskospringboot/TeluskospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mugen.teluskospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugen/teluskospringboot/TeluskospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugen.teluskospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeluskospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
